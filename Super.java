c1b25ae834dfbc03fdd2c431d0d74223ed71e681
Gunjan Kumari <112612648+gunjan6@users.noreply.github.com>
2023-03-29 22:07:30 +0530

    Add files via upload

---
+++ Super.java
@@ -0,0 +1,23 @@
+import javax.xml.transform.SourceLocator;
+
+public class Super{
+    public static void main(String[] args) {
+      Horse h = new Horse();  
+      System.out.println(h.color);
+    }
+}
+
+// Parent class creation
+class Animal{
+    String color;
+    Animal() {
+        System.out.println("Animal Constructor is called");
+    }
+}
+// Child class with super keyword
+class Horse extends Animal{
+    Horse(){
+        super.color="brown";
+        System.out.println("Horse constructor is called");
+    }
+}
