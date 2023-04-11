5517e98923bd16d1120addae6419f8f96b23a81b
Gunjan Kumari <112612648+gunjan6@users.noreply.github.com>
2023-04-11 23:35:36 +0530

    Add files via upload

---
+++ Constr.java
@@ -0,0 +1,28 @@
+public class Constr {
+   public static void main(String[] args) {
+    Student s1 = new Student();
+        s1.name = "Tony";
+        s1.roll = 5;
+        s1.password ="abc";
+    
+    Student s2 = new Student(s1);
+    s2.password = "xyz"; 
+    }
+}
+
+class Student{
+    String name ; 
+    int roll;
+    String password;
+
+    Student(Student s1){
+        this.name = s1.name; 
+        this.roll = s1.roll;        
+    }
+    Student(String Name){
+        name = Name;    
+    }
+    Student(){
+        System.out.println("Constructor is called.....");
+    }
+}
